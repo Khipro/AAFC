9cf6ac075b342ab45797b293c30af8f13be8a0d7
Khipro <kshiprabasu@gmail.com>
2020-03-24 14:38:43 -0400

    commit on master

--- raster_extension.py
+++ raster_extension.py
@@ -1,21 +1,48 @@
+"""
+Name: Pixel data extraction for raster files
+
+Description: Identify unique pixels for specified raster files. The code will print the 
+the list of unique cells - count of unique cells.
+
+Requirements:
+ 1. Numpy, Gdal(GDAL-3.0.4-cp38-cp38-win_amd64.whl) libraries
+ 2. The program was written on Python 3.8.0
+ 3. The raster file and the file location (D:\TESTfolder\2019_test_results\BC\LCV_BC_2019.pix)
+
+Author- Kshipra Basu
+Date- Mar 2020
+
+Note: This program is work-in progress with possibilities of being fine tuned.
+Recommendation: If required then the code can be modified to export results as csv.
+
+
+"""
 import os
 import warnings
 import numpy
 import scipy
 from osgeo import gdal, ogr, osr
 
-#Importing the raster file form location and validation
+
+#Specifying the Raster File location
 src_shp = r'D:\TESTfolder\2019_test_results\BC\LCV_BC_2019.pix' 
+
+#Use gdal to extract the data for the specific file.
 dataset = gdal.Open(src_shp, gdal.GA_ReadOnly)
+
+#Data validation of the input file
 if not dataset:
     print("Invalid input")
 
-#raster data count
+#Count raster pixel data
+#Get the unique pixel values
+#Count the values of specific pixel per category
 ds_ref = gdal.Open(src_shp)
 crop_ref_data = ds_ref.ReadAsArray()
 uni_class, uni_count = numpy.unique(crop_ref_data,return_counts=True)
 
 
-#formatting the comparison output
+#Removing the 0's from the results
+#Printing the extracted values
 Comparison = "\n".join("{} {}".format(x, y) for x, y in zip(uni_class[uni_class != 0], uni_count[uni_count !=0]))
 print (Comparison)
